8f2ced4b6ff0cb68e63a9e533cf2b44fb9adf4ee
AndDe-gourav <gourav.and.de@gmail.com>
2026-05-17 23:30:19 +0530

    typography chagnes

--- app/src/main/java/com/timepass/bookreader/ui/GeneralTopBar.kt
+++ app/src/main/java/com/timepass/bookreader/ui/GeneralTopBar.kt
@@ -1,22 +1,23 @@
 package com.timepass.bookreader.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.timepass.bookreader.R
 
@@ -30,40 +31,44 @@ fun GeneralTopBar(
         modifier = modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp),
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         Surface(
-            color = MaterialTheme.colorScheme.onBackground,
-            shape = RoundedCornerShape(topEnd = 8.dp, bottomEnd = 8.dp),
-            shadowElevation = 4.dp
+            color = Color.White,
+            onClick = onBackClicked,
+            shape = RoundedCornerShape(topEnd = 20.dp),
+            border = BorderStroke(width = 0.5.dp, color = MaterialTheme.colorScheme.tertiary)
         ) {
-            IconButton(onClick = onBackClicked) {
-                Icon(
-                    painter = painterResource(R.drawable.sign_out_circle),
-                    contentDescription = "Back",
-                    tint = MaterialTheme.colorScheme.inverseSurface,
-                    modifier = Modifier.size(24.dp)
-                )
-            }
+            Icon(
+                painter = painterResource(R.drawable.arrow_back_24dp_000000_fill0_wght300_grad0_opsz24),
+                contentDescription = "Back",
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)
+            )
         }
-
         Surface(
-            color = MaterialTheme.colorScheme.onBackground,
-            shape = RoundedCornerShape(topStart = 8.dp, bottomStart = 8.dp),
-            shadowElevation = 4.dp,
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 8.dp)
+            color = Color.White,
+            shape = RoundedCornerShape(bottomStart = 20.dp),
+            border = BorderStroke(width = 0.5.dp, color = MaterialTheme.colorScheme.tertiary),
+            modifier = Modifier.fillMaxWidth()
         ) {
             Text(
                 text = titleText,
-                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
-                color = MaterialTheme.colorScheme.inverseSurface,
+                style = MaterialTheme.typography.titleLarge,
                 textAlign = TextAlign.Center,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.padding(vertical = 13.dp, horizontal = 16.dp)
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun TopBarPreview() {
+    GeneralTopBar(
+        onBackClicked = {},
+        titleText = "Home"
+    )
 }
